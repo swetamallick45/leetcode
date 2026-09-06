8ce2c360d438722940b19742d7ccccf39cea2d0a
swetamallick45 <swetamallick23@gmail.com>
2026-09-06 22:19:49 +0530

    Time: 35 ms (40.71%) | Memory: 99.7 MB (87.35%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class MinStack {
+
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+        else {
+
+            minStack.push(minStack.peek());
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
